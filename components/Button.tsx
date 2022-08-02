13dd736e7b9675bda1cd9c917263b8180ff70c57
Angel Mario <savin@angelmario.eu>
2022-08-02 21:18:36 +0000

    Starting to look good

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,11 +4,11 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 
 export function Button(props: h.JSX.HTMLAttributes<HTMLButtonElement>) {
-  return (
-    <button
-      {...props}
-      disabled={!IS_BROWSER || props.disabled}
-      class={tw`px-2 py-1 border(gray-100 2) hover:bg-gray-200`}
-    />
-  );
+	return (
+		<button
+			{...props}
+			disabled={!IS_BROWSER || props.disabled}
+			class={tw`px-2 py-1 border(gray-100 2) hover:bg-gray-200`}
+		/>
+	);
 }
--- utils/twind.ts
+++ utils/twind.ts
@@ -1,8 +1,35 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup } from "twind";
 export * from "twind";
+
 export const config: Configuration = {
-  darkMode: "class",
-  mode: "silent",
+	mode: "silent",
+	darkMode: "media",
+	theme: {
+		extend: {
+			colors: {
+				primary: "#8267be",
+				secondary: "#D77fa1",
+				dark: "#212121",
+				paper: {
+					light: "#f0f0f0",
+					dark: "#323232",
+				},
+			},
+			borderRadius: {
+				DEFAULT: "0.95rem",
+			},
+			backdropBlur: {
+				DEFAULT: "16px",
+			},
+			container: {
+				center: true,
+			},
+			screens: {
+				"betterhover": { "raw": "(hover: hover)" },
+			},
+		},
+	},
 };
+
 if (IS_BROWSER) setup(config);
