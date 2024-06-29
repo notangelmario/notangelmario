f0cbd5e0c2ff97c602698728ff06f6db2ec0723e
Savin Angel-Mario <hello@notangelmario.dev>
2024-06-29 17:58:26 +0300

    Moved to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,21 @@
 import { defineConfig } from 'astro/config';
-
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	server: {
-		port: 3000
-	},
-	security: {
-		checkOrigin: true
-	},
-	site: "https://notangelmario.dev",
-	output: "hybrid",
-	adapter: cloudflare({
-		imageService: "compile"
-	}),
-	integrations: [tailwind({
-		applyBaseStyles: false
-	}), svelte()]
+  server: {
+    port: 3000
+  },
+  security: {
+    checkOrigin: true
+  },
+  site: "https://notangelmario.dev",
+  output: "hybrid",
+  adapter: vercel(),
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), svelte()]
 });
